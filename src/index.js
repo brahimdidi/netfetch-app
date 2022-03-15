c8f55a435d96e4210fb84a214e05e4debd14ed41
brahim <brahimgb1team@gmail.com>
2022-03-15 15:28:36 +0000

    link js files

--- src/index.js
+++ src/index.js
@@ -1,18 +1,3 @@
-import './style.css'
-
-const api1 = "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi";
-
-
-// styling 
-
-const navDiv = document.getElementById('nav-div')
-const mediaStyle = (x) => {
-    if (x.matches) { // If media query matches
-      navDiv.classList.remove('w-50')
-    }
-    return
-  }
-
-  
-  const x = window.matchMedia("(max-width: 700px)")
-  mediaStyle(x) // Call listener function at run time
+import './style.css';
+import './modules/createCard.js';
+import './modules/api.js'
