918c7aa9771748d4fa487e5be97f64300ddcbda4
vikipretium <vighneshpasupathy@gmail.com>
2022-03-16 20:49:42 +0530

    restructure folders

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import "./style.css";
-import "./modules/homepage/createCard.js";
-import "./modules/homepage/api.js";
-import "./modules/homepage/likeCounter.js";
+import "./modules/createCard.js";
+import "./modules/api.js";
+import "./modules/likeCounter.js";
