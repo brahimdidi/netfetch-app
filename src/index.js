60444a8dee17592d7ed7c66e54f3a05ba024d2ee
vikipretium <vighneshpasupathy@gmail.com>
2022-03-16 20:38:29 +0530

    restructure folders

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './style.css';
-import './modules/createCard.js';
-import './modules/api.js';
-import './modules/likeCounter'
+import "./style.css";
+import "./modules/homepage/createCard.js";
+import "./modules/homepage/api.js";
+import "./modules/homepage/likeCounter.js";
--- src/modules/homepage/createCard.js
+++ src/modules/homepage/createCard.js
@@ -1,9 +1,9 @@
-import getApi from './api.js';
+import getApi from "./api.js";
 
-const main = document.querySelector('#main');
-const createCard = (name, imageSrc,id) => {
-  const cardContainer = document.createElement('div');
-  cardContainer.classList.add('movie-card', 'card', 'gap-2', 'bg-dark');
+const main = document.querySelector("#main");
+const createCard = (name, imageSrc, id) => {
+  const cardContainer = document.createElement("div");
+  cardContainer.classList.add("movie-card", "card", "gap-2", "bg-dark");
   cardContainer.innerHTML = ` <img src="${imageSrc}"  alt="movie image" class ="movie-img p-2 bg-info">
     <div class="title-rating w-100 d-flex justify-content-around">
         <h4>${name}</h4>
@@ -13,15 +13,15 @@ const createCard = (name, imageSrc,id) => {
         </div>
     </div>
     <button type="button" class="btn btn-outline-light" data-bs-toggle="modal" data-bs-target="#exampleModal">Comments</button>
-    <button type="button" class="btn btn-outline-primary">Reservations</button>`;
-  
+    <button type="button" class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#reservation">Reservations</button>`;
+
   main.appendChild(cardContainer);
 };
 
 const movieList = async () => {
   const moviesData = await getApi();
   for (const movie of moviesData) {
-    createCard(movie.name, movie.image.medium,movie.id);
+    createCard(movie.name, movie.image.medium, movie.id);
   }
 };
 
