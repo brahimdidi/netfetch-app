41711c2c5a8679e8900751aa852b6d15aba1684c
brahim <brahimgb1team@gmail.com>
2022-03-18 17:11:54 +0000

    print user comment to ui

--- src/modules/printComment.js
+++ src/modules/printComment.js
@@ -1,13 +1,14 @@
 import { setComment, getComment } from './comments.js';
-// console.log(getComment)
 
 const commentBtn = document.querySelector('.comment-btn');
 const list = document.querySelector('.list-group');
 const listGroupItem = document.querySelector('.list-group-item');
-
+const commentsCounter = document.querySelector('.counter'); 
 const data = (id) => {
-  console.log(id);
    getComment(id).then((res) => {
+    let counter = 0
+    res.forEach((el) => counter += 1);
+    commentsCounter.innerHTML = `${counter}`;
     printComment(list, res);
   });
 };
@@ -19,7 +20,7 @@ const printComment = (list, res) => {
     let item = document.createElement('li');
     item.classList.add('list-group-item')
       // console.log(el)
-    item.innerHTML = `${el.creation_date} ${el.username} ${el.comment}`;
+    item.innerHTML = `${el.creation_date}  ${el.username} : ${el.comment}`;
     list.appendChild(item);
   });
 
