485e8c9a5808ce1b6ff0babf7bc755c2ab3980a0
brahim <brahimgb1team@gmail.com>
2022-03-18 17:12:19 +0000

    print user reserv to ui

---
+++ src/modules/printReservation.js
@@ -0,0 +1,27 @@
+import { getReservation } from "./reservation.js";
+
+
+const list = document.querySelector(".output-reservation");
+const reservationCounter = document.querySelector('.counter-reservation'); 
+
+
+
+const dataReservation = (id) => {
+  getReservation(id).then((res) => {
+    let counterReserv = 0
+    res.forEach((el) => counterReserv += 1);
+    reservationCounter.innerHTML = `${counterReserv}`;
+    printReservation(list, res);
+  });
+};
+export default dataReservation;
+
+const printReservation = (list, res) => {
+  list.innerHTML = "";
+  res.forEach((el) => {
+    let item = document.createElement("li");
+    item.classList.add("list-group-item");
+    item.innerHTML = ` ${el.date_start} - ${el.date_end} ${el.username}`;
+    list.appendChild(item);
+  });
+};
