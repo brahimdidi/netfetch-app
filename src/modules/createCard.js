ab0b8c9b27c2317fc861321069ef9e5b34a15a79
brahim <brahimgb1team@gmail.com>
2022-03-18 08:56:08 +0000

    update create card functon

--- src/modules/createCard.js
+++ src/modules/createCard.js
@@ -1,28 +1,24 @@
-import getApi from './api.js';
+// import getApi from './api.js';
 
-const main = document.querySelector('#main');
-const createCard = (name, imageSrc,id) => {
-  const cardContainer = document.createElement('div');
-  cardContainer.classList.add('movie-card', 'card', 'gap-2', 'bg-dark');
-  cardContainer.innerHTML = ` <img src="${imageSrc}"  alt="movie image" class ="movie-img p-2 bg-info">
-    <div class="title-rating w-100 d-flex justify-content-around">
-        <h4>${name}</h4>
-        <div class="like-div">
-        <span id="like-number">10</span>
-        <i id="${id}" class="fa-solid fa-heart"></i>
-        </div>
+const createCard = (moviesData,itemLike ={}) => ` <div class="movie-card card gap-2 bg-dark" id ="${moviesData.id}">
+<img  alt="movie image" class ="movie-img p-2 bg-info" src = "${moviesData.image.medium}">
+<div class="title-rating w-100 d-flex justify-content-around">
+    <h4 class ="text-light">${moviesData.name}</h4>
+    <div class="like-div">
+    <span class="likes-counter">${moviesData.name}</span>
+    <p class="likes"> <i class="fas fa-heart like-icon" data-id="${moviesData.id}"> <span class="num"> ${itemLike} </span> </i>  likes </p>
     </div>
-    <button type="button" class="btn btn-outline-light" data-bs-toggle="modal" data-bs-target="#exampleModal">Comments</button>
-    <button type="button" class="btn btn-outline-primary">Reservations</button>`;
-  
-  main.appendChild(cardContainer);
-};
+</div>
+<button type="button" class="btn btn-outline-light">Comments</button>
+<button type="button" class="btn btn-outline-primary">Reservations</button>
+</div> `;
+// export const displayMovies= async () => {
+//   const moviesData = await getApi();
+//   for (const movie of moviesData) {
+//     createCard(movie.name, movie.image.medium,movie.id);
+//   }
+// };
 
-const movieList = async () => {
-  const moviesData = await getApi();
-  for (const movie of moviesData) {
-    createCard(movie.name, movie.image.medium,movie.id);
-  }
-};
+export default createCard;
 
-movieList();
+
