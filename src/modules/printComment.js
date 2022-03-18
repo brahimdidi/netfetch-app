f063128baddbe4c30776205d2bfc76d612910cde
ridwan <ridwanediallo99@gmail.com>
2022-03-18 23:00:56 +0100

    Add jest test

--- src/modules/printComment.js
+++ src/modules/printComment.js
@@ -4,10 +4,19 @@ import { setComment, getComment } from './comments.js';
 const commentBtn = document.querySelector('.comment-btn');
 const list = document.querySelector('.list-group');
 const listGroupItem = document.querySelector('.list-group-item');
+const commentsCounter = document.querySelector('.counter');
+
+export const counterComments = (data) => {
+  let counter = 0;
+  data.forEach((el) => (counter += 1));
+  return counter;
+};
+
 
 const data = (id) => {
-  console.log(id);
-   getComment(id).then((res) => {
+  getComment(id).then((res) => {
+    commentsCounter.innerHTML = `${counterComments(res)}`;
+
     printComment(list, res);
   });
 };
@@ -17,17 +26,10 @@ const printComment = (list, res) => {
   list.innerHTML = '';
   res.forEach((el) => {
     let item = document.createElement('li');
-    item.classList.add('list-group-item')
-      // console.log(el)
+    item.classList.add('list-group-item');
+    // console.log(el)
     item.innerHTML = `${el.creation_date} ${el.username} ${el.comment}`;
     list.appendChild(item);
   });
-
 };
 
-
-
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     data();
-//   });
