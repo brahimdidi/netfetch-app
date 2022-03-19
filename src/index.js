bc2e0809623817e47b72c92d5aee27a41ca73b7d
vikipretium <vighneshpasupathy@gmail.com>
2022-03-19 17:00:36 +0530

    fix hint linters

--- src/index.js
+++ src/index.js
@@ -1,30 +1,31 @@
-import './style.css';
-import createCard from './modules/createCard.js';
-import './modules/style.js';
-import getData, { postLikes, getLikesApi } from './modules/api.js';
-import getMovieItem from './modules/renderComment.js';
-import './modules/printComment.js';
-import getMovieItemReservation from './modules/renderReservation.js';
+import "./style.css";
+import { Tooltip, Toast, Popover } from "bootstrap";
+import createCard from "./modules/createCard.js";
+import "./modules/style.js";
+import getData, { postLikes, getLikesApi } from "./modules/api.js";
+import getMovieItem from "./modules/renderComment.js";
+import "./modules/printComment.js";
+import getMovieItemReservation from "./modules/renderReservation.js";
 
-const main = document.querySelector('#main');
-const movieBtnHeader = document.getElementById('movies-button');
+const main = document.querySelector("#main");
+const movieBtnHeader = document.getElementById("movies-button");
 
 const loading = () => {
-  const loadDiv = document.createElement('div');
-  const mask = document.createElement('div');
-  loadDiv.classList.add('loading');
-  mask.classList.add('mask');
+  const loadDiv = document.createElement("div");
+  const mask = document.createElement("div");
+  loadDiv.classList.add("loading");
+  mask.classList.add("mask");
   main.append(mask, loadDiv);
 };
 
 const removeLoding = () => {
-  document.querySelector('.loading').remove();
-  document.querySelector('.mask').remove();
+  document.querySelector(".loading").remove();
+  document.querySelector(".mask").remove();
 };
 
 // renderItems function start
 
-if (main.innerHTML === '') {
+if (main.innerHTML === "") {
   loading();
 } else {
   removeLoding();
@@ -32,7 +33,7 @@ if (main.innerHTML === '') {
 
 const renderItems = async () => {
   const likesData = await getLikesApi();
-  main.innerHTML = '';
+  main.innerHTML = "";
   const moviesData = await getData();
   moviesData.forEach((item) => {
     let count = 0;
@@ -47,37 +48,38 @@ const renderItems = async () => {
     movieBtnHeader.textContent = `Movies (${moviesData.length})`;
   });
 
-  const like = document.querySelectorAll('.like-icon');
+  const like = document.querySelectorAll(".like-icon");
 
   like.forEach((item) => {
-    item.addEventListener('click', () => {
-      const movieId = item.getAttribute('data-id');
+    item.addEventListener("click", () => {
+      const movieId = item.getAttribute("data-id");
 
-      if (item.style.color !== 'grey') {
-        item.style.color = 'grey';
-        item.firstElementChild.innerHTML = Number(item.firstElementChild.innerHTML) + 1;
+      if (item.style.color !== "grey") {
+        item.style.color = "grey";
+        item.firstElementChild.innerHTML =
+          Number(item.firstElementChild.innerHTML) + 1;
         postLikes(movieId);
       }
     });
   });
   // target the comment button
-  const trigerModal = document.querySelectorAll('.triger-modal');
+  const trigerModal = document.querySelectorAll(".triger-modal");
   trigerModal.forEach((btn) => {
-    btn.addEventListener('click', (e) => {
-      const tagetMovi = e.target.getAttribute('identifier');
+    btn.addEventListener("click", (e) => {
+      const tagetMovi = e.target.getAttribute("identifier");
       getMovieItem(tagetMovi, moviesData, e.target.id);
-      const commentBtn = document.querySelector('.comment-btn');
+      const commentBtn = document.querySelector(".comment-btn");
       commentBtn.id = e.target.id;
     });
   });
 
   // target the reservation button
-  const trigerModalReserv = document.querySelectorAll('.triger-modal-reserv');
+  const trigerModalReserv = document.querySelectorAll(".triger-modal-reserv");
   trigerModalReserv.forEach((btn) => {
-    btn.addEventListener('click', (e) => {
-      const reservTarget = e.target.getAttribute('identifier');
+    btn.addEventListener("click", (e) => {
+      const reservTarget = e.target.getAttribute("identifier");
       getMovieItemReservation(reservTarget, moviesData, e.target.id);
-      const reservationBtn = document.querySelector('.reservation-btn');
+      const reservationBtn = document.querySelector(".reservation-btn");
       reservationBtn.id = e.target.id;
     });
   });
